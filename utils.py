f6c6f087c874882b2ba7eaa298fcc3432e995356
MartinH2k3 <martin.horsky.2003@gmail.com>
2025-03-09 13:04:23 +0100

    Added a way to use deepseek. Changed system prompt so it doesnt generate JSONs like json```[]```

--- utils.py
+++ utils.py
@@ -19,8 +19,11 @@ with open(_system_prompt_path, "r") as f:
     _system_prompt = f.read()
 _model_path = _config.get("model_path")
 _checkpoint_path = _config.get("checkpoint_path")
-_api_model = _config.get("api_model")
+_gpt_model = "gpt-4o-mini"
+_deepseek_model = "deepseek-chat"
 _result_separator = _config.get("result_separator")
+_litellm_url = "http://147.175.151.44/"
+_deepseek_url = "https://api.deepseek.com"
 
 def get_system_prompt():
     return _system_prompt
@@ -107,23 +110,32 @@ def generate_response(user_input: str, model=None, tokenizer=None):
     generation_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
     return generation_pipeline(prepared_input, **generation_args)[0]['generated_text']
 
-client: Optional[openai.OpenAI] = None
+_gpt_client: Optional[openai.OpenAI] = None
 def load_client():
-    global client
-    if client is not None:
-        return
+    global _gpt_client
+    if _gpt_client is not None:
+        return _gpt_client
     load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
-    litellm_url = os.getenv("LITELLM_URL")
-    client = openai.OpenAI(api_key=api_key, base_url=litellm_url)
+    _gpt_client = openai.OpenAI(api_key=api_key, base_url=_litellm_url)
+    return _gpt_client
 
+_deepseek_client: Optional[openai.OpenAI] = None
+def load_deepseep_client():
+    global _deepseek_client
+    if _deepseek_client is not None:
+        return _deepseek_client
+    load_dotenv()
+    api_key = os.getenv("DEEPSEEK_API_KEY")
+    _deepseek_client = openai.OpenAI(api_key=api_key, base_url=_deepseek_url)
+    return _deepseek_client
 
-def generate_openai(user_input: str):
-    load_client()
+def generate_openai(user_input: str, use_deepseek: bool = False):
+    client = load_client() if not use_deepseek else load_deepseep_client()
     if client is None:
         raise ValueError("Client not loaded")
     response = client.chat.completions.create(
-        model=_api_model,
+        model=_gpt_model if not use_deepseek else _deepseek_model,
         messages=[
             {"role": "system", "content": get_system_prompt()},
             {"role": "user",
@@ -133,7 +145,7 @@ def generate_openai(user_input: str):
     return response.choices[0].message.content
 
 
-def store_results(file_path: str, results: list[str], do_backup: bool = False, do_append: bool = False):
+def store_results(results: list[str], file_path: str, do_backup: bool = False, do_append: bool = False):
     with open(file_path, 'w' if not do_append else 'a') as file:
         file.write(_result_separator.join(results))
 
@@ -141,3 +153,8 @@ def store_results(file_path: str, results: list[str], do_backup: bool = False, d
         from datetime import datetime
         with open(file_path + '_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.txt', 'w') as file:
             file.write(_result_separator.join(results))
+
+
+def load_results(file_path: str):
+    with open(file_path, 'r') as file:
+        return file.read().split(_result_separator)
