d3dceeb40972d5356a31be0f81b4072a26f706ec
MartinH2k3 <martin.horsky.2003@gmail.com>
2025-03-28 19:20:34 +0100

    Restructured calling models from API

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -12,18 +12,15 @@ from dotenv import load_dotenv
 import os
 from typing import Optional
 
-with open('config.json', 'r') as file:
+with open('../config.json', 'r') as file:
     _config = json.load(file)
 _system_prompt_path = _config.get("system_prompt_path")
 with open(_system_prompt_path, "r") as f:
     _system_prompt = f.read()
 _model_path = _config.get("model_path")
 _checkpoint_path = _config.get("checkpoint_path")
-_gpt_model = "gpt-4o-mini"
 _deepseek_model = "deepseek-chat"
 _result_separator = _config.get("result_separator")
-_litellm_url = "http://147.175.151.44/"
-_deepseek_url = "https://api.deepseek.com"
 
 def get_system_prompt():
     return _system_prompt
@@ -110,32 +107,23 @@ def generate_response(user_input: str, model=None, tokenizer=None):
     generation_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
     return generation_pipeline(prepared_input, **generation_args)[0]['generated_text']
 
-_gpt_client: Optional[openai.OpenAI] = None
-def load_client():
-    global _gpt_client
-    if _gpt_client is not None:
-        return _gpt_client
+_openai_client: Optional[openai.OpenAI] = None
+def load_openai_client(provider: str = "litellm"):
+    global _openai_client
+    if _openai_client is not None:
+        return _openai_client
     load_dotenv()
-    api_key = os.getenv("OPENAI_API_KEY")
-    _gpt_client = openai.OpenAI(api_key=api_key, base_url=_litellm_url)
-    return _gpt_client
+    api_key = os.getenv(f"{provider.upper()}_API_KEY")
+    _openai_client = openai.OpenAI(api_key=api_key, base_url=_config.get(f"{provider.lower()}_url"))
+    return _openai_client
 
-_deepseek_client: Optional[openai.OpenAI] = None
-def load_deepseep_client():
-    global _deepseek_client
-    if _deepseek_client is not None:
-        return _deepseek_client
-    load_dotenv()
-    api_key = os.getenv("DEEPSEEK_API_KEY")
-    _deepseek_client = openai.OpenAI(api_key=api_key, base_url=_deepseek_url)
-    return _deepseek_client
 
-def generate_openai(user_input: str, use_deepseek: bool = False):
-    client = load_client() if not use_deepseek else load_deepseep_client()
+def generate_openai(user_input: str, provider: str = "litellm", model_name: str = "gpt-4o-mini"):
+    client = load_openai_client(provider)
     if client is None:
         raise ValueError("Client not loaded")
     response = client.chat.completions.create(
-        model=_gpt_model if not use_deepseek else _deepseek_model,
+        model= model_name,
         messages=[
             {"role": "system", "content": get_system_prompt()},
             {"role": "user",
