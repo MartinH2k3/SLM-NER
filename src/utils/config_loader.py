38d0834c44d3aa10d56b3128d52919dc0744dd77
MartinH2k3 <martin.horsky.2003@gmail.com>
2025-04-10 22:30:05 +0200

    Renamed model_path for phi-3 mini to main_model in config

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -3,10 +3,10 @@ import json
 
 def get_project_root():
     # Assuming utils/ is always one level down from project root and I didn't change anything
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 
-def load_config(filename="config.json", use_local_model=False):
+def load_config(filename="config.json"):
     project_root = get_project_root()
     config_path = os.path.join(project_root, filename)
 
@@ -15,11 +15,7 @@ def load_config(filename="config.json", use_local_model=False):
         config = json.load(f)
 
     for key, value in config.items():
-        if key.endswith("_path") and key != "model_path":
-            if not os.path.isabs(value):
-                config[key] = os.path.join(project_root, value)
-        elif key == "model_path" and use_local_model:
-            if not os.path.isabs(value):
+        if key.endswith("_path") and not os.path.isabs(value):
                 config[key] = os.path.join(project_root, value)
 
     return config
